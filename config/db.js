408d59a15a1dc0461c4f87d85d38995f572cca06
hari1-addania <108327333+hari1-addania@users.noreply.github.com>
2023-05-31 19:06:51 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose"
 
-import { MongoClient } from 'mongodb';
-const client = new MongoClient(process.env.MONGO_URI);
+
+
 
 
 const connectDB = async()=>{
     try {
-        const conn =  await client.connect(process.env.MONGO_URI)
+        const conn =  await mongoose.connect(process.env.MONGO_URI)
         console.log(`connected to mongoDB ${conn.connection.host}`)
     } catch (error) {
         console.log(`error in mongodb ${error}`)
